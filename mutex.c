7da87bacac35fcd6da030b8534197f1af5ba3d4d
Jeevan Sandhu <jeevansandhu95@gmail.com>
2020-03-13 15:34:32 -0700

    Mutex updates

--- mutex.c
+++ mutex.c
@@ -15,8 +15,10 @@ struct thread{
 
 struct semaphore{
   int lock;
-  void* acquire;
-  void* release;
+  void (*acquire)(const struct semaphore*);
+  void (*release)(const struct semaphore*);
+//  void* acquire;
+//  void* release;
 };
 
 int nexttid;                            // Next value for tid
@@ -132,36 +134,42 @@ void pong(){
   uthread_exit();
 }
 
-void sem_init(){
-  sem.lock = 0;
-  counter = 0;
-  void (*sem().acquire)(int) = &acquire_lock;
-  void (*sem().release)(int) = &release_lock;
-  
-}
-
-void acquire_lock(){
-  while(sem.lock != 0){
+void acquire_lock(const struct semaphore* sem){
+  while(sem->lock != 0){
   }
 
-  sem.lock = 1;
+  sem->lock = 1;
   return;
 }
 
-void release_lock(){
-  sem.lock = 0;
+void release_lock(const struct semaphore* sem){
+  sem->lock = 0;
   return;
+}
+
+void sem_init(){
+  struct semaphore *s;
+  s = &sem;
+  s->lock = 0;
+  s->acquire = acquire_lock;
+  s->release = release_lock;
+//  sem.lock = 0;
+  counter = 0;
+//  void (*acquire_ptr)(int) = &acquire_lock;
+//  void (*release_ptr)(int) = &release_lock;
+//  sem.acquire = acquire_ptr;
+//  sem.release = release_ptr;
 }
 
 
 void lock(){
   //Acquire lock
-  sem().acquire;
+  sem.acquire(&sem);
   printf(1,"Thread #%d has acquired the lock\n", threadtable[curthread].tid);
   counter++;
   printf(1,"Value of the shared variable is currently %d\n", counter);
   //Release lock
-  sem().release;
+  sem.release(&sem);
   uthread_exit();
 }
 
